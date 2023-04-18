91a92ec6218b87847d15c0272735171afed83733
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-04-18 14:42:43 -0400

    application controller

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :users, only:[:create,:new,:destroy]
+
+  resource :session, only:[:create,:destroy,:new]
+
+
 end
