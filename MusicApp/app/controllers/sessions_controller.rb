7edb4ea5ba3d5ec7b4adb0306c9566fee644071c
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-04-18 15:31:01 -0400

    sessions controller

--- MusicApp/app/controllers/sessions_controller.rb
+++ MusicApp/app/controllers/sessions_controller.rb
@@ -1,2 +1,40 @@
 class SessionsController < ApplicationController
+ before_action :require_logged_in, only:[:destroy]
+ before_action :require_logged_out, only:[:create,:new]
+
+ def new 
+    @user = User.new 
+
+    render :new 
+ end
+
+ def create 
+    username = params[:user][:username]
+    password = params[:user][:password]
+
+    @user = self.find_by(username,password)
+
+    if @user 
+        login!(@user)
+        redirect_to user_url(@user)
+
+    else
+
+        @user = User.new(username: username)
+        render :new
+    end
+
+
+end
+
+
+def destroy 
+    logout! 
+
+    redirect_to new_session_url
+end
+
+
+
+    
 end
--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
     def self.find_by_credentials(username,password)
         user = User.find_by(username: username)
 
-        if user && user.is_password?(password)
+        if user && user.is_password(password)
             return user 
 
         else
-            return nil 
+            nil
         end
     end
 
@@ -22,22 +22,25 @@ class User < ApplicationRecord
         bcrypt_obj.is_password?(password)
     end
 
-    def generate_session_token 
-         SecureRandom::urlsafe_base64
+    def generate_session_token
+        SecureRandom::urlsafe_base64
     end
 
     def password=(password)
-        self.password_digest = Bcrypt::Password.create(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
     end
 
     def ensure_session_token 
-        self.session_token||= generate_session_token 
+        self.session_token||= generate_session_token
     end
 
-    def reset_session_token 
+    def reset_session_token! 
         self.session_token = generate_session_token
         self.save!
-        self.session_token
+        self.session_token 
     end
+
+
 
 end
